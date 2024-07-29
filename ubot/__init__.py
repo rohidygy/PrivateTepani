e2ac4728bed9670d14d34e995e29e8d679cc70e6
Madurainhire <108894773+rohidygy@users.noreply.github.com>
2024-07-29 12:47:47 +0700

    Update __init__.py

--- ubot/__init__.py
+++ ubot/__init__.py
@@ -14,6 +14,7 @@ from pyrogram.types import *
 from ubot.config import *
 from pyromod import listen
 from telegraph.aio import Telegraph
+from pytgcalls import GroupCallFactory
 
 aiosession = ClientSession()
 
