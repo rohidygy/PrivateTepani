97ee0cd3243ad0b15ee7a9d50411564de8d19ba5
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-08-02 00:50:04 +0700

    update ubot/modules/vctools.py

--- ubot/modules/vctools.py
+++ ubot/modules/vctools.py
@@ -75,18 +75,17 @@ async def leavevc(client, message):
     chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
     with suppress(ValueError):
         chat_id = int(chat_id)
-    turun = await client.call_py.stop()
-    if turun:
-        await message.reply(f"<emoji id =5974045315391556490>📝</emoji> **anda sedang tidak berada di dalam obrolan suara manapun**.")
-    else:
-        try:
-            await client.call_py.leave_call(message.chat.id)
-        except Exception as e:
-            return await message.reply(f"ERROR: {e}")
-        msg = f"**❏ Berhasil Meninggalkan Voice Chat <emoji id=5798623990436074786>✅</emoji>**\n"
-        if chat_id:
-            msg += f"**╰ Chat**: {message.chat.title}"
-        await message.reply(msg)
+    # turun = await client.call_py.stop()
+    # if turun:
+    #     await message.reply(f"<emoji id =5974045315391556490>📝</emoji> **anda sedang tidak berada di dalam obrolan suara manapun**.")
+    try:
+        await client.call_py.leave_call(message.chat.id)
+    except Exception as e:
+        return await message.reply(f"ERROR: {e}")
+    msg = f"**❏ Berhasil Meninggalkan Voice Chat <emoji id=5798623990436074786>✅</emoji>**\n"
+    if chat_id:
+        msg += f"**╰ Chat**: {message.chat.title}"
+    await message.reply(msg)
 
 @PY.UBOT("startvc")
 async def opengc(client: Client, message: Message):
