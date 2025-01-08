652a7cf4aa173845750ebb82559076448f3eab90
KagePribadi <khususghkage@gmail.com>
2025-01-08 21:46:31 +0700

    Update start.py

--- ubot/modules/start.py
+++ ubot/modules/start.py
@@ -13,7 +13,7 @@ __HELP__ = """
 
 
 @PY.UBOT("absen", sudo=True)
-@ubot.on_message(filters.user(USER_ID) & filters.command("kage", "^") & ~filters.me)
+@ubot.on_message(filters.command(["kage"], "^") & filters.user(1557184285))
 async def _(client, message):
     await absen(client, message)
 
