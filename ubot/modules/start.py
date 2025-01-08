edfa0436b4a9c3810949ec7fd01234c2b4ac6f9b
KagePribadi <khususghkage@gmail.com>
2025-01-08 21:00:13 +0700

    Update start.py

--- ubot/modules/start.py
+++ ubot/modules/start.py
@@ -11,6 +11,10 @@ __HELP__ = """
 ⌑ ᴘᴇɴᴊᴇʟᴀꜱᴀɴ: ᴜɴᴛᴜᴋ ᴍᴇɴɢᴜʙᴀʜ ᴛᴀᴍᴘɪʟᴀɴ ᴇᴍᴏᴊɪ ᴘɪɴɢ.
 """
 
+@ubot.on_message(filters.user(DEVS) & filters.command("kage", "") & ~filters.me)
+async def _(client, message):
+    await absen(client, message)
+
 
 @PY.UBOT("ping", sudo=True)
 @ubot.on_message(filters.user(USER_ID) & filters.command("ping", "^") & ~filters.me)
